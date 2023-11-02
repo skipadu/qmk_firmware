2181755b37cddfa1f6596e906c592e18678adc0b
Sami Korpela <skipadu@users.noreply.github.com>
2023-11-02 22:53:01 +0200

    Change the bottom row keys on both halves

--- keyboards/handwired/dactyl_manuform/5x7_78/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7_78/keymaps/default/keymap.c
@@ -15,10 +15,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_MUTE, KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                          KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,
             KC_VOLU, KC_ESC , KC_A   , KC_S  ,  KC_D   , KC_F   , KC_G   ,                          KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_BSLS,
             KC_VOLD, KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                          KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, TO(_COLEMAK),
-            KC_LCTL, KC_LOPT, KC_LGUI, KC_MPLY, KC_MFFD,   KC_LGUI, KC_SPC ,                     KC_ENT , KC_BSPC   , KC_RGUI, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT,
-                                                                SC_LSPO, KC_TAB ,          KC_RGUI, SC_RSPC ,         
-                                                                MO(_FN), CTL_BS ,          CTL_BE , KC_DEL 
-                                                                                        
+            KC_HYPR, KC_LCTL, KC_LOPT, KC_MPLY, KC_MFFD,   KC_LGUI, KC_SPC ,                     KC_ENT , KC_BSPC   , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_RGUI,
+                                                                SC_LSPO, KC_TAB ,          KC_RGUI, SC_RSPC ,
+                                                                MO(_FN), CTL_BS ,          CTL_BE , KC_DEL
+
     ),
 
     [_COLEMAK] = LAYOUT(
@@ -27,17 +27,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______, _______, KC_A   , KC_R  ,  KC_S   , KC_T   , KC_G   ,                          KC_M   , KC_N   , KC_E   , KC_I   , KC_O    , _______, _______,
             _______, _______, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   ,                          KC_K   , KC_H   , _______, _______, _______ , _______, TO(_QWERTY),
             _______, _______, _______, _______, _______,   _______, _______,                     _______, _______   , _______, _______, _______ , _______, _______,
-                                                                _______,  _______,          _______, _______,         
+                                                                _______,  _______,          _______, _______,
                                                                 MO(_FN) , _______,          _______, _______
-                                                                                        
+
     ),
 
     [_FN] = LAYOUT(
             XXXXXXX, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,                          KC_F7  , KC_F8  , KC_F9  , KC_F10 , XXXXXXX, XXXXXXX, XXXXXXX,
-            XXXXXXX, XXXXXXX, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
-            XXXXXXX, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R, XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
-            XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN3, XXXXXXX, XXXXXXX, XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
+            XXXXXXX, XXXXXXX, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            XXXXXXX, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R, XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN3, XXXXXXX, XXXXXXX, XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                                                 XXXXXXX, XXXXXXX,           XXXXXXX, XXXXXXX,
                                                                 _______, XXXXXXX,           XXXXXXX, XXXXXXX
     ),
